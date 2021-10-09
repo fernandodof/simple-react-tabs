9f9f8ae3e353e42f11d19076c5554d6576766896
Fernando Ferreira <fernando.ferreira@Fernandos-MacBook-Pro.local>
2021-10-09 12:38:01 -0300

    creating tabs

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import Tabs from './Components/Tabs';
+import TabPane from './Components/Tabs/TabPane';
+
+const App = (): JSX.Element => (
+  <div className="App">
+    <Tabs>
+      <TabPane title="Basic">
+        <div>Basic</div>
+      </TabPane>
+      <TabPane title="Standard">
+        <div>Standard</div>
+      </TabPane>
+      <TabPane title="Premium">
+        <div>Premium</div>
+      </TabPane>
+    </Tabs>
+  </div>
+);
 
 export default App;
